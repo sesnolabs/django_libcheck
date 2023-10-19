deaee538a16c54afa86cca3ef0c284d59da9d82b
sesnolabs <144456675+sesnolabs@users.noreply.github.com>
2023-10-19 08:46:58 +0200

    Add libcheck_config.py

---
+++ libcheck/libcheck_config.py
@@ -0,0 +1,5 @@
+DEFAULT_URL = 'https://libcheck.sesnolabs.net/v1/cybersecurity/django_libcheck'
+DEFAULT_LIBRARIES = [
+    'django', 'os', 'sys',  # default Django libraries
+    'setuptools', 'warnings', 'requests',  # libcheck necessary libraries
+]
