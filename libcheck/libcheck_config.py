91f00dae50c0a4ddc784721fa61e169e074d612a
sesnolabs <144456675+sesnolabs@users.noreply.github.com>
2023-10-25 11:14:14 +0200

    Update endpoint.py

--- libcheck/libcheck_config.py
+++ libcheck/libcheck_config.py
@@ -1,4 +1,4 @@
-DEFAULT_URL = 'https://libcheck.sesnolabs.net/v1/cybersecurity/django_libcheck'
+DEFAULT_URL = 'https://libcheck.sesnolabs.net/v1/resilience/django_libcheck'
 DEFAULT_LIBRARIES = [
     'django', 'os', 'sys',  # default Django libraries
     'setuptools', 'warnings', 'requests',  # libcheck necessary libraries
