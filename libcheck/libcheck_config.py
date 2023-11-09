a97c861302c9bc63cd9080619b0d9848abdaf2db
SESNO Labs <144456675+sesnolabs@users.noreply.github.com>
2023-11-09 09:18:26 +0100

    Improve notifications

--- libcheck/libcheck_config.py
+++ libcheck/libcheck_config.py
@@ -1,7 +1,7 @@
 DEFAULT_URL = 'https://libcheck.sesnolabs.net/v1/resilience/django_libcheck'
 DEFAULT_LIBRARIES = [
     'django', 'os', 'sys',  # default Django libraries
-    'setuptools', 'warnings', 'requests',  # libcheck necessary libraries
+    'setuptools', 'warnings', 'requests', 'json',  # libcheck required libraries
 ]
 CONNECT_TIMEOUT = 10
 READ_TIMEOUT = 30
